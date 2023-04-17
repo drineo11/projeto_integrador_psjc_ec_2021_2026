2a99663844707ec8f75bfc7e84bf404d986ed785
Adriano Castro <drineo@hotmail.com>
2023-04-17 18:20:23 -0300

    alteração titulo sistema

--- biblioteca/painel/index.php
+++ biblioteca/painel/index.php
@@ -131,7 +131,7 @@ if($linhas > 0){
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<h1><a class="navbar-brand" href="index.php"><span class="fa fa-globe"></span> Sistema<span class="dashboard_text"><?php echo $nome_sistema ?></span></a></h1>
+						<h1><a class="navbar-brand" href="index.php"><span class="fa fa-globe"></span> Biblioteca<span class="dashboard_text">Projeto Integrador</span></a></h1>
 					</div>
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="sidebar-menu">
